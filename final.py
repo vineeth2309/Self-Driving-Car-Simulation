9d903d92e4012276dcd4647fe88be7830651e3bd
Vineeth S Subramanyam <19423039+vineeth2309@users.noreply.github.com>
2018-10-10 22:22:20 +0530

    Add files via upload

--- final.py
+++ final.py
@@ -1,138 +1,280 @@
-import numpy as np
-from PIL import ImageGrab
 import cv2
-import tensorflow as tf
-import math
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 
-def f(x):
-    return x
 
-kernel = np.ones((5,5),np.float32)/25
-kernel2 = np.ones((5,5) , np.uint8)
 
-linelength = []
+cap=cv2.VideoCapture("challenge_video.mp4")
 
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('o1utput.avi',fourcc, 20.0, (844,480))
+
+kernel = np.ones((5,5),np.uint8)
+sobel_kernel = 15
+thresh_dir_min = 0.7
+thresh_dir_max = 1.2
+thresh_mag_min = 50
+thresh_mag_max = 255
+
+srcleft = np.float32([[215,315],[400,315],[270,435],[0,376]])
+dstleft = np.float32([[0, 0],[750, 0],[750,480],[0, 480]])
+
+srcmid = np.float32([[315,315],[575,315],[775,430],[100,430]])
+dstmid = np.float32([[0, 0],[840, 0],[840,480],[0, 480]])
+
+srcright = np.float32([[440,315],[595,315],[820,380],[560,430]])
+dstright = np.float32([[0, 0],[750, 0],[750,480],[0, 480]])
+
+left_lane=[]
+right_lane=[]
+nwindows = 10
+margin = 100
+minpix = 50
+window_height = 48
 set1=0
+ploty = np.linspace(0, 479, 480)
+y_eval = np.max(ploty)
+img_size = (844,480)
 
-
-def HoughLines(img,processed_image):
-    lines = cv2.HoughLinesP(img,1,np.pi/180,80,np.array([]),20,300)
-    lineneedssorting=draw_lines(processed_image,lines)
-    length_sorted = sorted(lineneedssorting, reverse=True)
-    return length_sorted
-
-def draw_lines(img,lines):
-    try:    
-        for line in lines:
-            coords=line[0]
-            cv2.line(img,(coords[0],coords[1]),(coords[2],coords[3]),(255,255,255),10)
-            #print('1')
-            l1 =(pow((coords[2]-coords[0]),2) + pow((coords[3]-coords[1]),2))
-            final_length=math.sqrt(l1)
-            linelength.append(final_length)
-    except:
-        pass
-    return linelength
-
-def average_slope_intercept(lines):
-    left_lines    = [] 
-    left_weights  = [] 
-    right_lines   = [] 
-    right_weights = []
-    try: 
-        for line in lines:
-            for x1,y1,x2,y2 in line:
-                if (x1==x2):
-                    continue
-                #y=mx+c
-                else:
-                    m = ((y2-y1)/(x2-x1))
-                    c = (y1 - (m*x1))
-                    line_length = np.sqrt(pow((y2-y1),2)+pow((x2-x1),2))
-                    if m < 0:
-                        left_lines.append((m,c))
-                        left_weights.append((line_length))
-                    else:
-                        right_lines.append((m,c))
-                        right_weights.append((line_length))
-    except:
-        pass
-    ######LEFT LINE AND RIGHT LINES SEPARATED REMOVING VERTICAL LINES####
-    left_lane  = np.dot(left_weights,  left_lines) /np.sum(left_weights)  if len(left_weights) >0 else None
-    right_lane = np.dot(right_weights, right_lines)/np.sum(right_weights) if len(right_weights)>0 else None
-    print(left_lane)
-    return left_lane, right_lane
-
-##def make_line_points(y1, y2, line):
-##    if line is None:
-##        return None
-##    
-##    slope, intercept = line
-##    
-##    # make sure everything is integer as cv2.line requires it
-##    if (slope==0):
-##        pass
-##    else:
-##        x1 = int((y1 - intercept)/slope)
-##        x2 = int((y2 - intercept)/slope)
-##        y1 = int(y1)
-##        y2 = int(y2)
-##        
-##    return ((x1, y1), (x2, y2))
-
-def lane_lines(image, lines):
-    left_lane, right_lane = average_slope_intercept(lines)
+def lane_finding(binary_warped_B):
     
-    y1 = image.shape[0] # bottom of the image
-    y2 = y1*0.6         # slightly lower than the middle
+    histogram = np.sum(binary_warped_B[binary_warped_B.shape[0]//2:,:], axis=0)
+    midpoint = np.int(histogram.shape[0]/2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+    nonzero = binary_warped_B.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    left_lane_inds = []
+    right_lane_inds = []
+    set1=0
+    for window in range(nwindows):
 
-    #left_line  = make_line_points(y1, y2, left_lane)
-    #right_line = make_line_points(y1, y2, right_lane)
-    #return left_line, right_line
+        win_y_low = binary_warped_B.shape[0] - (window+1)*window_height
+        win_y_high = binary_warped_B.shape[0] - window*window_height
 
-def roi(image,vertices):
-    mask = np.zeros_like(image)
-    cv2.fillPoly(mask,vertices,255)
-    masked = cv2.bitwise_and(image,mask)
-    return masked
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+        #print(good_left_inds)
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+
+##        if len(good_left_inds) > minpix :
+##            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+##            print(leftx_current)
+##            set1=set1+1
+##            print(set1)
+##        if len(good_right_inds) > minpix:        
+##            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
     
-def process_image(original_image):
+    #print(left_lane_inds)
+   
+    leftx = nonzerox[left_lane_inds] 
+    lefty = nonzeroy[left_lane_inds] 
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
 
-    lowerwhite = np.array([200,200,200])
-    upperwhite = np.array([255,255,255])
-    loweryellow = np.array([60,0,100])
-    upperyellow = np.array([255,255,255])
-
-    frameHLS=cv2.cvtColor(original_image,cv2.COLOR_BGR2HLS)
-    #frameHSV=cv2.cvtColor(original_image,cv2.COLOR_BGR2LAB)
-
-    mask_white=cv2.inRange(original_image,lowerwhite,upperwhite)
-    mask_yellow=cv2.inRange(frameHLS,loweryellow,upperyellow)
-    mask = cv2.bitwise_or(mask_white,mask_yellow)
-
-    res=cv2.bitwise_and(original_image,original_image,mask=mask)
-    new_image1 = cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
     
-    blur = cv2.GaussianBlur(new_image1,(15,15),0)
-    dilation = cv2.dilate(blur,kernel2,iterations = 1)
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
 
-    edges = cv2.Canny(dilation,40,150,apertureSize=3)
+    return ploty, left_fitx, right_fitx, left_fit, right_fit,leftx,rightx,lefty,righty
 
-    return edges
 
-while True:
+def radius_pixels(ploty, left_fit, right_fit):
+
+
+    left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
+    right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
     
-    printscreen_pil = ImageGrab.grab(bbox=(0,10,640,480))
-    processed_image = process_image(np.array(printscreen_pil))
-    vertices = np.array([[50,375],[200,275],[400,275],[620,375],[620,450],[50,450]])
-    new_image=roi(processed_image,[vertices])
+    radius_pix = (left_curverad + right_curverad)//2
 
-    sorted_lines=HoughLines(new_image,processed_image)
+    return radius_pix
+
+def radius_m(leftx, rightx, ploty, y_eval):
     
+    ym_per_pix = 30/720 
+    xm_per_pix = 370/700 
+    left_fit_cr = np.polyfit(leftx*ym_per_pix, (leftx*xm_per_pix), 2)
+    right_fit_cr = np.polyfit(rightx*ym_per_pix, rightx*xm_per_pix, 2)
 
-    cv2.imshow('graywindow',processed_image)
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
 
+    radius = (left_curverad + right_curverad)//2
+
+    return radius
+
+
+def offset_cal_pix(left_fitx, right_fitx, image,lefty,righty):
+    bottom=[]
+    top=[]
+      
+    left_pointbottomx = int(left_fitx[len(left_fitx) - 10])
+    right_pointbottomx = int(right_fitx[len(right_fitx) - 10])
+    left_pointbottomy = int(lefty[len(lefty)-10])
+    right_point_bottomy = int(righty[len(righty)-10])
+    
+    left_pointtopx = int(left_fitx[10])
+    right_pointtopx = int(right_fitx[10])
+    left_pointtopy = int(lefty[10])
+    right_pointtopy = int(lefty[10])
+
+    
+    bottommidx = int((right_pointbottomx-left_pointbottomx)/2)
+    topmidx = int((right_pointtopx-left_pointtopx)/2)
+    bottommidy = int((right_point_bottomy+left_pointbottomy)/2)
+    topmidy = int((right_pointtopy+left_pointtopy)/2)
+    bottom=(bottommidx,bottommidy)
+    top = (topmidx,topmidy)
+    
+    lane_width = right_pointbottomx - left_pointbottomx    
+    lane_center = ((left_pointbottomx) + (lane_width / 2))    
+    vehicle_center = (image.shape[1] / 2)
+    offset = vehicle_center - lane_center
+    return offset, lane_width, lane_center,bottom,top
+
+def offset_cal_met(offset,lane_width):
+    xm_per_pix = 3.0/700   
+    offset_met = round(xm_per_pix*offset, 4)
+    lane_width = round(xm_per_pix*lane_width, 4)    
+    return offset_met,lane_width
+
+
+def augument(binary_warped_B, left_fitx, right_fitx, ploty, Minv_B, img, undistorted, lane_width, prev_pts,color,bottom,top):
+
+    warp_zero = np.zeros_like(binary_warped_B).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    if lane_width <= 750:
+        
+        pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+        #pts_mid = np.vstack((pointsleft,pointsright)).astype(np.int32).T
+        
+        pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+        pts = np.hstack((pts_left, pts_right))
+       
+        if len(pts)>0:
+            cv2.fillPoly(color_warp, np.int_([pts]), color)
+            cv2.line(color_warp,bottom,top,(255,0,0),50)
+            prev_pts = pts
+    else:
+        pts = prev_pts
+        if len(pts)>0:
+            cv2.fillPoly(color_warp, np.int_([prev_pts]), color)
+            cv2.line(color_warp,bottom,top,(255,0,0),50)
+    cv2.imshow("a",color_warp)
+    newwarp = cv2.warpPerspective(color_warp, Minv_B, (img.shape[1], img.shape[0])) 
+    result = cv2.addWeighted(undistorted, 1, newwarp, 0.3, 0)   
+    prev_pts = pts
+    return result, prev_pts
+
+
+
+def perspective_transform(img,src,dst):   
+    img_size = (img.shape[1], img.shape[0])    
+    m = cv2.getPerspectiveTransform(src, dst)
+    m_inv = cv2.getPerspectiveTransform(dst, src)
+    warped = cv2.warpPerspective(img, m, img_size, flags=cv2.INTER_LINEAR)
+    return warped,m_inv
+
+
+
+
+def lane(image,d,frame):
+    
+    prev_pts = []   
+    
+    frameLAB=cv2.cvtColor(image,cv2.COLOR_BGR2LAB)
+    frameHLS=cv2.cvtColor(image,cv2.COLOR_BGR2HLS)
+    frameYCrCb=cv2.cvtColor(image,cv2.COLOR_BGR2YCrCb)
+    (Y,Cr,Cb) = cv2.split(frameYCrCb) 
+    (B, G, R) = cv2.split(image)
+    (L ,A, B1) = cv2.split(frameLAB)
+    (H,L1,S1) = cv2.split(frameHLS)
+    blurr = cv2.medianBlur(R,7)
+    blurl = cv2.medianBlur(L,7)
+    blury = cv2.medianBlur(Y,7)
+    blurs = cv2.medianBlur(S1,7)
+
+    rgb_r_thresh = cv2.adaptiveThreshold(blurr, 255, adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C, thresholdType=cv2.THRESH_BINARY, blockSize=25, C=-15)
+    lab_l_thresh = cv2.adaptiveThreshold(blurl, 255, adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C, thresholdType=cv2.THRESH_BINARY, blockSize=25, C=-30)
+    hls_s_thresh = cv2.adaptiveThreshold(blurs, 255, adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C, thresholdType=cv2.THRESH_BINARY, blockSize=25, C=-20)
+    ycrcb_y_thresh = cv2.adaptiveThreshold(blury, 255, adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C, thresholdType=cv2.THRESH_BINARY, blockSize=25, C=-25)
+    lab_l_thresh_noise = cv2.adaptiveThreshold(blurl, 255, adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C, thresholdType=cv2.THRESH_BINARY, blockSize=65, C=10)
+
+    
+    noise_mask = cv2.inRange(blurl, 135, 255)
+    noise_bool = np.logical_or(np.logical_not(noise_mask), lab_l_thresh_noise)
+    noise_mask = np.zeros_like(blurr, dtype=np.uint8)
+    noise_mask[noise_bool] = 255
+    m1=np.logical_or(rgb_r_thresh,hls_s_thresh)
+    merged_bool = np.logical_and(np.logical_or(m1, lab_l_thresh,ycrcb_y_thresh), noise_mask)
+    merged1=np.logical_or(merged_bool,m1)
+    merged = np.zeros_like(R, dtype=np.uint8)
+    merged[merged1] = 255
+
+    ploty, left_fitx, right_fitx, left_fit, right_fit, leftx, rightx, lefty, righty = lane_finding(merged)
+    radius_pix = radius_pixels(ploty, left_fit, right_fit)
+    radius = radius_m(leftx, rightx, ploty, y_eval)
+    
+    
+    
+    return merged, left_fitx, right_fitx, d, prev_pts, lefty, righty    
+            
+    
+   
+while(cap.isOpened()):
+
+    ret,frame=cap.read()
+    start_time = time.time()
+    
+    aleft,dleft = perspective_transform(frame,srcleft,dstleft)
+    amid,dmid = perspective_transform(frame,srcmid,dstmid)
+    aright,dright = perspective_transform(frame,srcright,dstright)
+
+    mergedm, left_fitx2, right_fitx2, d2, prev_pts2,leftmidy,rightmidy = lane(amid,dmid,frame)
+    offsetmid, lane_widthmid,lane_centermid,midbottom,midtop = offset_cal_pix(left_fitx2, right_fitx2, amid,leftmidy,rightmidy)
+    offset_metmid,lmid = offset_cal_met(offsetmid,lane_widthmid)
+    
+    mergedr, left_fitx1, right_fitx1, d1, prev_pts1,rightlefty,rightrighty = lane(aright,dright,frame)
+    offsetright, lane_widthright,lane_centerright,rightbottom,righttop = offset_cal_pix(left_fitx1, right_fitx1, aright,rightlefty,rightrighty)
+    offset_metright,lright = offset_cal_met(offsetright,lane_widthright)
+    
+    mergedl, left_fitx3, right_fitx3, d3, prev_pts3,leftlefty,leftrighty = lane(aleft,dleft,frame)
+    offsetleft, lane_widthleft,lane_centerleft,leftbottom,lefttop = offset_cal_pix(left_fitx3, right_fitx3, aleft,leftlefty,leftrighty)
+    offset_metleft,lleft = offset_cal_met(offsetleft,lane_widthleft)
+
+    offleft = (lmid/2)+(lleft/2)+offset_metleft
+    offright = (lmid/2)+(lright/2)+offset_metright
+    
+    set1=set1+1
+    print(set1)
+    augumented1,prev_pts11 = augument(mergedr, left_fitx1, right_fitx1, ploty, d1,frame, frame, lane_widthright, prev_pts1,(0,255, 255),rightbottom,righttop)
+    augumented2,prev_pts22 = augument(mergedm, left_fitx2, right_fitx2, ploty, d2, augumented1, augumented1, lane_widthmid, prev_pts2,(0,255, 0),midbottom,midtop)
+    augumented3,prev_pts33 = augument(mergedl, left_fitx3, right_fitx3, ploty, d3, augumented2, augumented2, lane_widthleft, prev_pts3,(0,255, 255),leftbottom,lefttop)
+    #print("FPS: ", 1.0 / (time.time() - start_time))
+    cv2.imshow('Result', augumented3)
+    #out.write(augumented3)
+    
+    
     k=cv2.waitKey(1)
-    if k == ord('q'):
-        break
-
+    if k==ord('q'):
+        break                         
+   
+cv2.destroyAllWindows()
+cap.release()
+out.release()
